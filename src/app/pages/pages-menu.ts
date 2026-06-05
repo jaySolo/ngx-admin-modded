a11a7c2b89db60eeadcfc21aa567adf7fd4eca2d
Jamal J. A. Solomon <jj_solomon16@live.com>
2026-06-05 03:38:40 -0400

    Added permissions template for menu items

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -6,16 +6,31 @@ export const MENU_ITEMS: NbMenuItem[] = [
     icon: 'monitor-outline',
     link: '/pages/dashboard',
     home: true,
+    permissions: null,
   },
   // add other menu items here
   {
     title: 'User Management',
     icon: 'people-outline',
     link: '/pages/security/users',
+    // permissions: {
+    //   combination: 'all',
+    //   list: [
+    //     { resource: 'plugin::users-permissions::user', access: 'find' },
+    //     { resource: 'plugin::users-permissions::user', access: 'findOne' },
+    //   ],
+    // },
   },
   {
     title: 'User Role Management',
     icon: 'people-outline',
     link: '/pages/security/roles',
+    // permissions: {
+    //   combination: 'all',
+    //   list: [
+    //     { resource: 'plugin::users-permissions::role', access: 'find' },
+    //     { resource: 'plugin::users-permissions::role', access: 'findOne' },
+    //   ],
+    // },
   },
 ];
