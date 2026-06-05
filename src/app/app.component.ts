4d41b0b5c3dcb7d1b88bd3aed74e6181326d08ea
Jamal J. A. Solomon <jj_solomon16@live.com>
2026-06-05 03:41:45 -0400

    Implement user authentication and loading state
    Add user authentication check and loading state management.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,30 @@ import { SeoService } from './@core/utils/seo.service';
   template: '<router-outlet></router-outlet>',
 })
 export class AppComponent implements OnInit {
+  
+  isLoaded: boolean = false;
 
-  constructor(private analytics: AnalyticsService, private seoService: SeoService) {
+  
+  constructor(
+    private analytics: AnalyticsService,
+    private seoService: SeoService,
+    auth: NbAuthService,
+    userStore: UserStore,
+    initService: InitUserService,
+  ) {
+    auth.isAuthenticated().subscribe(isLoggedIn => {
+      if (isLoggedIn === true) {
+        if (!userStore.getUser()) {
+          initService.initCurrentUser().subscribe((user) => {
+            this.isLoaded = user?.id > 0;
+          });
+        }
+      } else {
+        this.isLoaded = true;
+      }
+    });
   }
+  
 
   ngOnInit(): void {
     this.analytics.trackPageViews();
